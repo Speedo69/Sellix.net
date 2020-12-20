42e4681ea0117f8a06b53c6e9a3b55761088f969
Speedo69 <traktormeelis2@gmail.com>
2020-12-20 17:12:08 +0200

    .

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 ApiKey=$1
-dotnet pack ./Sellix.net/Sellix.Net.csproj -c Release
+dotnet pack ./Sellix.net/Sellix.Net.csproj -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 cd ./Sellix.net/bin/Release
 dotnet nuget push "*.nupkg" -k $ApiKey -s https://api.nuget.org/v3/index.json --skip-duplicate
