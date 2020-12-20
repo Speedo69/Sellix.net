f5a41ca6f9eeeb69388bc21f446911e2d79474d2
Speedo69 <traktormeelis2@gmail.com>
2020-12-20 14:16:24 +0200

    Token.

---
+++ Example/Token.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example
+{
+    public static class Token
+    {
+        public static string GetToken() => "xD7Wu9DzvbTd6xVmZPQ96sL0dq8bpJjTvPQ3aD7fySLWzSq2nrThzBKtPKzDXoso";
+    }
+}
---
+++ Tests/Token.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    public class Token
+    {
+        //This is a token connected to a dummy account, don't waste your time with it.
+        public static string Get => "mIsbTr35QTBaZYixvtaEy692jveBvgAOT3ntC5Xx5dr0du0fgswZYuxsV9o1ySow";
+    }
+}
